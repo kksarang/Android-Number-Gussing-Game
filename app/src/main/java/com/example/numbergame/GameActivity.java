1b51eb4442816369eb07fc2f576ea5534e029034
kksarang <sarangrajan17@gmail.com>
2022-01-11 22:55:35 +0530

    firstlast final

--- app/src/main/java/com/example/numbergame/GameActivity.java
+++ app/src/main/java/com/example/numbergame/GameActivity.java
@@ -1,7 +1,10 @@
 package com.example.numbergame;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -9,6 +12,7 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
@@ -23,6 +27,11 @@ public class GameActivity extends AppCompatActivity {
     int random;
 
     int remainingRight = 10;
+
+
+    ArrayList<Integer> guessList = new ArrayList<>();
+    int userAttempts = 0;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,16 +85,59 @@ public class GameActivity extends AppCompatActivity {
                     textViewHint.setVisibility(View.VISIBLE);
 
                     remainingRight--;
+                    userAttempts++;
+
 
 
                     int userGuss = Integer.parseInt(guss);
 
-                    textViewLast.setText(guss);
+                    guessList.add(userGuss);
 
-                    textViewRight.setText(remainingRight);
+
+                    textViewLast.setText("Your last Guss is :"+guss);
+
+                    textViewRight.setText("Your Remain right is :"+remainingRight);
 
                     if (random == userGuss)
                     {
+
+                        AlertDialog.Builder builder = new
+                                AlertDialog.Builder(GameActivity.this);
+
+
+                        builder.setTitle("Number Gussing Game");
+                        builder.setCancelable(false);
+
+                        builder.setMessage("Congiguration. My guess was "+random +" \n \n You Know My Number In"+
+                                userAttempts+" attempts . \n\n Your Guess :"+guessList
+                        +"\n\n Would You Like To Play Again ?");
+
+
+                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i)
+                            {
+                                Intent intent = new Intent(GameActivity.this,MainActivity.class);
+                                startActivity(intent);
+                                finish();
+
+                            }
+                        });
+
+                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i)
+                            {
+                               moveTaskToBack(true);
+
+                               android.os.Process.killProcess(android.os.Process.myPid());
+
+                               System.exit(1);
+
+                            }
+                        });
+
+                        builder.create().show();
 
                     }
                     if (random < userGuss)
@@ -101,6 +153,44 @@ public class GameActivity extends AppCompatActivity {
                     if (remainingRight == 0)
                     {
 
+                        AlertDialog.Builder builder = new
+                                AlertDialog.Builder(GameActivity.this);
+
+
+                        builder.setTitle("Number Gussing Game");
+                        builder.setCancelable(false);
+
+                        builder.setMessage("Sorry Your right to Guss is over "
+                                + "\n \n My Guss Was :"+random
+                                + "\n\n Your Guess :"+guessList
+                                + "\n\n Would You Like To Play Again ?");
+
+
+                        builder.setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i)
+                            {
+                                Intent intent = new Intent(GameActivity.this,MainActivity.class);
+                                startActivity(intent);
+                                finish();
+
+                            }
+                        });
+
+                        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i)
+                            {
+                                moveTaskToBack(true);
+
+                                android.os.Process.killProcess(android.os.Process.myPid());
+
+                                System.exit(1);
+
+                            }
+                        });
+
+                        builder.create().show();
                     }
 
                     editTextGuss.setText("");
